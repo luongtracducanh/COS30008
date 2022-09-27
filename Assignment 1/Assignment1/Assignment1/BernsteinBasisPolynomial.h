19c5b229ec33d8727229bbe4f4699983f1537b11
luongtracducanh <luongtrac.ducanh@gmail.com>
2022-09-28 00:10:43 +0700

    add asm 1

---
+++ Assignment 1/Assignment1/Assignment1/BernsteinBasisPolynomial.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Combination.h"
+// https://en.wikipedia.org/wiki/Bernstein_polynomial
+class BernsteinBasisPolynomial
+{
+private:
+	Combination fFactor;
+
+public:
+
+	// constructor for b(v,n) with defaults
+	BernsteinBasisPolynomial(unsigned int aV = 0, unsigned int aN = 0);
+
+	// call operator to calculate Berstein base
+	// polynomial for a given x (i.e., aX)
+	double operator()(double aX) const;
+};
